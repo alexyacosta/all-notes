703b108ceddd7a408ac75310564fdb18f404a451
Alexya de Sousa Costa <alexyasc@primeit.com.br>
2022-05-23 10:44:14 -0300

    ajustes finais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,9 +21,14 @@ import { TelaTraducaoComponent } from './views/tela-traducao/tela-traducao.compo
     TelaFotosComponent,
     TelaTraducaoComponent
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserAnimationsModule
   ],
   providers: [],
